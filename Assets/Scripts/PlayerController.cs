3ccd007d5a4fe5030f5119d3874202e09bd9df56
manhiem <abhi160302@gmail.com>
2023-05-25 19:20:28 +0530

    Jump and Crouch Animations Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public BoxCollider2D playerCollider;
+    private Vector2 colliderOffset;
+    private Vector2 colliderSize;
     // Start is called before the first frame update
     void Start()
     {
-        
+        colliderOffset = playerCollider.offset;
+        colliderSize = playerCollider.size;
     }
 
     // Update is called once per frame
     void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         animator.SetFloat("Speed", Mathf.Abs(speed));
+        animator.SetFloat("Vertical", Mathf.Abs(vertical));
 
+        // Move Animation
         Vector3 scale = transform.localScale;
         if(speed < 0) {
             scale.x = -1.0f * Mathf.Abs(scale.x);
@@ -25,5 +32,20 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+        // Crouch Animation
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            speed = 0;
+            animator.SetBool("Crouch", true);
+            playerCollider.offset = new Vector2(-0.1658681f, 0.5918809f);
+            playerCollider.size = new Vector2(0.8149413f, 1.301477f);
+        }
+        if(speed!=0 || Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", false);
+            playerCollider.offset = colliderOffset;
+            playerCollider.size = colliderSize;
+        }
     }
 }
