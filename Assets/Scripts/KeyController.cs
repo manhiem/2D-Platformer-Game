af144ddc538f757e1292b9e7f2b7f08f7609ee14
manhiem <abhi160302@gmail.com>
2023-06-13 06:49:38 +0530

    Added Collectibles

---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeyController : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public int score;
+
+    private void Awake()
+    {
+        score = 0;
+        IncrementScore();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            score += 10;
+            IncrementScore();
+            Destroy(gameObject);
+        }
+    }
+
+    private void IncrementScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
