cc900eadd08329dc57d75dd345d3b937b7687688
manhiem <abhi160302@gmail.com>
2023-06-17 16:15:54 +0530

    Added Level Selection

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    //public LobbyManager lobbyManager;
+    public void RestartGame()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.SetActiveScene(scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LobbyManager : MonoBehaviour
+{
+    public string level;
+    public static LobbyManager Instance { get; private set; }
+
+    public GameObject levelPanel;
+    public GameObject startPanel;
+
+    //public TextMeshProUGUI restartBtn;
+    //public TextMeshProUGUI quitBtn;
+    private void Awake()
+    {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void setLevel(string levelName)
+    {
+        level = levelName;
+        StartLevel(level);
+    }
+
+    public void RestartGame(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+    public void StartLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+    public void StartGame()
+    {
+        startPanel.SetActive(false);
+        levelPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
