f5ee55181237605b34a9168453b0bb8b5dcc21f3
manhiem <abhi160302@gmail.com>
2023-06-17 10:15:40 +0530

    Added Lobby

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Collided!!! Ending the level!");
+        }
+    }
+}
---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
