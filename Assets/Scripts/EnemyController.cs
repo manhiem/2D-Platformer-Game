380d6e37c9126f78c2982edf3e3bfbc70ff709cf
manhiem <abhi160302@gmail.com>
2023-06-22 00:49:29 +0530

    Added Audios

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public Transform leftPoint;
+    public Transform rightPoint;
+
+    private bool movingRight = true;
+
+    private void Update()
+    {
+        // Move Animation and Physics
+        Vector3 scale = transform.localScale;
+        if (movingRight)
+        {
+            scale.x = 1.0f;
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            scale.x = -1.0f;
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
+        transform.localScale = scale;
+
+        CheckEdges();
+    }
+
+    private void CheckEdges()
+    {
+        if (movingRight && transform.position.x > rightPoint.position.x)
+        {
+            movingRight = false;
+        }
+        else if (!movingRight && transform.position.x < leftPoint.position.x)
+        {
+            movingRight = true;
+        }
+    }
+}
